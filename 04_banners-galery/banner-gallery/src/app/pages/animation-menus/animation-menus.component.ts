c9020988f7f365b4586612f2de8ef0761ec12ec3
Scarus <41557299+Eduard20CR@users.noreply.github.com>
2022-02-24 15:46:59 -0600

    Data animations added

--- 04_banners-galery/banner-gallery/src/app/pages/animation-menus/animation-menus.component.ts
+++ 04_banners-galery/banner-gallery/src/app/pages/animation-menus/animation-menus.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SiblingCommunicationService } from 'src/app/shared/services/sibling-communication/sibling-communication.service';
-
+import animationArray from './../../../assets/data/animations/animations.json'
 @Component({
   selector: 'app-animation-menus',
   templateUrl: './animation-menus.component.html',
@@ -10,6 +10,9 @@ export class AnimationMenusComponent implements OnInit {
   type = 'In';
   typeArray = ['', ''];
   open = false;
+  animationMenuData;
+  actualMenu=0;
+
   constructor(private siblingsComunication: SiblingCommunicationService) {}
 
   ngOnInit(): void {
@@ -17,5 +20,6 @@ export class AnimationMenusComponent implements OnInit {
     this.siblingsComunication.sendAnimationType.subscribe((e) => {
       this.type = e;
     });
+    this.animationMenuData = animationArray[0].animations[this.actualMenu];
   }
 }
